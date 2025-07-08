72e85b2374d642b130bcf63379e3f5db064175cd
shaik sohail ahmmad <shaiksohailahmmad777@gmail.com>
2025-07-08 13:57:19 +0530

    added css to app.jsx
    created navbar component
    
    added navbar for situations like when user is logged in and whenhe logged out
    
    added css to them

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,17 @@ import Home from './pages/Home'
 import BuyCredit from './pages/Buycredit'
 import Result from './pages/Result'
 import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b frm-teal-50 to-orange-50'>
+      <Navbar />
       <Routes>
 
-        <Home />
-        <BuyCredit />
-        <Result />
+        <Route path='/' element={<Home />} />{/*here we had kept path="/" as this thing indicates that its the home page */}
+        <Route path='/result' element={<Result />} />
+        <Route path='/buy' element={<BuyCredit />} />
         
       </Routes>
 
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import {assets} from '../assets/assets'
+import { Link, useNavigate } from 'react-router-dom'
+const Navbar = () => {
+
+    const [user, setuser] =useState(true);//to identify weather user is logged in or logged out so, depending upon that we can give differnt navbars and other things
+
+    const navigate = useNavigate();//its simliar to link tag only which is used to go from one page to another
+  return (
+    <div className='flex items-center justify-between py-4'>
+
+      <Link to="/" >
+      <img src={assets.logo} alt='' className='w-28 sm:w-32 lg:w-40' />
+      </Link>
+    {/*we should always add another div inside the main div only not outside the main div its an rule */}
+      <div>
+        {
+        user ?
+        <div className='flex items-center gap-2 sm:gap-3 '>
+            <button className='flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700'>
+                <img className='w-5' src={assets.credit_star} alt="" />
+                <p className='text-xs sm:text-sm font-medium text-gray-600'>Credits left : 50</p>
+            </button>
+            <p className='text-gray-600 max-sm:hidden pl-4'>Hi, Sohail</p>
+            <div className='relative group'>
+                <img src={assets.profile_icon} className='w-10 drop-shadow' />
+                <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
+                    <ul className='list-none  m-0 p-2 bg-white rounded-md border text-sm'>
+                        <li className=''>Logout</li>
+                    </ul>
+                </div>
+            </div>
+        </div> 
+        : 
+        <div className='flex items-center gap-2 sm:gap-5'>
+          <p onClick={()=>navigate('/buy')}className='cursor-pointer'>Pricing</p>
+          <button className='bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full'>Login</button>
+        </div>
+        }
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
